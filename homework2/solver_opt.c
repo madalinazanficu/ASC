38db68db8e075db5d6eb8757821d92cc978604e3
madalinazanficu <zanficumadalina@gmail.com>
2023-05-06 14:36:31 +0300

    Solver opt in progress 5

--- homework2/solver_opt.c
+++ homework2/solver_opt.c
@@ -38,27 +38,7 @@ double *get_transpose(double *matrix, int N) {
 double *multiply_superior(double *A, double *B, int N) {
 
 	double *M = (double *)calloc(N * N, sizeof(double));
-
-	// for (int i = 0; i < N; i++) {
-	// 	for (int j = 0; j < N; j++) {
-
-	// 		register double sum = 0;
-	// 		double *a = A + i * N + i;
-	// 		double *b = B + i * N + j;
-
-	// 		for (int k = i; k < N; k++) {
-	// 			// sum += *(A + i * N + k) * *(B + k * N + j);
-	// 			sum += *a * *b;
-	// 			a++;
-	// 			b += N;
-	// 		}
-
-	// 		*(m + i * N + j) = sum;
-	// 	}
-	// }
 	for (int i = 0; i < N; i++) {
-		// register int m_line = i * N;
-
 		for (int k = i; k < N; k++) {
 			register double *a = A + i * N + k;
 			register double *b = B + k * N;
@@ -76,28 +56,9 @@ double *multiply_superior(double *A, double *B, int N) {
 }
 
 /* B inferior triunghiulara */
-double *multiply_inferior(double *A, double *B, int N) {
+void multiply_inferior(double *A, double *B, double *M, int N) {
 
-	double *M = (double *)calloc(N * N, sizeof(double));
-
-	// for (int i = 0; i < N; i++) {
-	// 	for (int j = 0; j < N; j++) {
-
-	// 		register double sum = 0;
-	// 		register double *a = A + i * N + j;
-	// 		register double *b = B + j * N + j;
-
-	// 		for (int k = j; k < N; k++) {
-	// 			// *(m + i * N + j) += *(A + i * N + k) * *(B + k * N + j);
-
-	// 			sum += *a * *b;
-	// 			a++;
-	// 			b += N;
-	// 		}
-	// 		*(m + i * N + j) = sum;
-	// 	}
-	// }
-
+	// double *M = (double *)calloc(N * N, sizeof(double));
 
 
 	for (int i = 0; i < N; i++) {
@@ -113,33 +74,11 @@ double *multiply_inferior(double *A, double *B, int N) {
 			}
 		}
 	}
-
-	return M;
 }
 
-double *multiply_normal(double *A, double *B, int N) {
+void multiply_normal(double *A, double *B, double *M, int N) {
 
-	double *M = (double *)calloc(N * N, sizeof(double));
-
-	// for (int i = 0; i < N; i++) {
-	// 	for (int j = 0; j < N; j++) {
-	// 		*(m + i * N + j) = 0;
-
-	// 		register double sum = 0;
-	// 		double *a = A + i * N;
-	// 		double *b = B + j;
-
-	// 		for (int k = 0; k < N; k++) {
-	// 			// *(m + i * N + j) += *(A + i * N + k) * *(B + k * N + j);
-
-	// 			sum += *a * *b;
-	// 			a++;
-	// 			b += N;
-	// 		}
-	// 		*(m + i * N + j) = sum;
-	// 	}
-	// }
-
+	// double *M = (double *)calloc(N * N, sizeof(double));
 	for (int i = 0; i < N; i++) {
 		for (int k = 0; k < N; k++) {
 			register double *a = A + i * N + k;
@@ -154,13 +93,13 @@ double *multiply_normal(double *A, double *B, int N) {
 		}
 	}
 
-	return M;
+	// return M;
 }
-
 
 
 /*
  * Add your optimized implementation here
+ * Renunt la 3 alocari de memorie
  */
 double* my_solver(int N, double *A, double* B) {
 	printf("OPT SOLVER\n");
@@ -170,24 +109,29 @@ double* my_solver(int N, double *A, double* B) {
 
 	// C = A * B * At + Bt * Bt
 	double *AB = multiply_superior(A, B, N);
-	double *ABAt = multiply_inferior(AB, At, N);
-	double *BtBt = multiply_normal(Bt, Bt, N);
 
-	// Add the two matrices
-	double *C = (double *)malloc(N * N * sizeof(double));
+
+	// A va contine ABAt 
+	multiply_inferior(AB, At, A, N);
+
+
+	// B va contine BtBt
+	multiply_normal(Bt, Bt, B, N);
+
+	// AB va contine adundarea C = ABAt + BtBt
+	// double *C = (double *)malloc(N * N * sizeof(double));
 	for (int i = 0; i < N * N; i++) {
-		C[i] = ABAt[i] + BtBt[i];
+		AB[i] = A[i] + B[i];
 	}
-
 
 	// Final step : free all auxiliar matrices used
 	free_matrix(&At);
 	free_matrix(&Bt);
-	free_matrix(&AB);
-	free_matrix(&ABAt);
-	free_matrix(&BtBt);
+	// free_matrix(&AB);
+	//free_matrix(&ABAt);
+	//free_matrix(&BtBt);
 
-	return C;	
+	return AB;	
 }
 
 
@@ -195,25 +139,25 @@ double* my_solver(int N, double *A, double* B) {
 	TODO: delete main
 */
 
-// int main() {
+int main() {
 
-// 	int N = 3;
+	int N = 3;
 
-// 	double A1[3][3] = {{1.0, 2.0, 3.0}, {0.0, 4.0, 5.0}, {0.0, 0.0, 6.0}};
-// 	double B1[3][3] = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
-
-
-// 	double *A = &A1[0][0];
-// 	double *B = &B1[0][0];
+	double A1[3][3] = {{1.0, 2.0, 3.0}, {0.0, 4.0, 5.0}, {0.0, 0.0, 6.0}};
+	double B1[3][3] = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
 
 
-// 	double *C = my_solver(N, A, B);
-// 	if (C == NULL) {
-// 		printf("C is NULL\n");
-// 	} else {
-// 		print_matrix(C, N);
-// 		free(C);
-// 	}
+	double *A = &A1[0][0];
+	double *B = &B1[0][0];
 
-// 	return 0;
-// }
+
+	double *C = my_solver(N, A, B);
+	if (C == NULL) {
+		printf("C is NULL\n");
+	} else {
+		print_matrix(C, N);
+		free(C);
+	}
+
+	return 0;
+}
